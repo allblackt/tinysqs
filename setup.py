ea965488e4be5029643c7152741fc6d7bdbf90b5
Tudor Tabacel <tudor.tabacel@avira.com>
2015-10-17 20:01:25 +0200

    First commit

---
+++ setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+install_requires = ['requests >= 1.2.0']
+
+tests_require = ['pytest', 'pytest-cov']
+
+setup(name='tinysqs',
+      version='0.0.1',
+      description=("A small library for interacting with SQS queues: getting "
+                   "messages, sending messages."),
+
+      author='Tudor Tabacel',
+      author_email='tudorsmt@gmail.com',
+      url='https://github.com/allblackt/tinysqs',
+      packages=['tinysqs'],
+      license='MIT',
+      classifiers=[
+          # make sure to use :: Python *and* :: Python :: 3 so
+          # that pypi can list the package on the python 3 page
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 3'
+      ],
+
+      platforms='Any',
+      keywords=('amazon', 'aws', 'sqs'),
+
+      package_dir={'': '.'},
+      install_requires=install_requires,
+      tests_require=tests_require,
+      test_suite='tinysqs.tests')
